292944722b33571db9a7f5a9cd2a927e21edc5c6
Whong Hao Ze <haozewhong@gmail.com>
2026-04-08 13:42:54 +0800

    Added loop feature in new branch

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, Git!" << endl;
+   for(int i = 1; i <= 3; ++i) {
+        cout << "Hello, Git! This is line " << i << endl;
+   }
     return 0;
 }
